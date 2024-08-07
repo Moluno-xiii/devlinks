29e49de7a8595118b55209685a52ce21faab9b89
Moluno-xiii <molunoprogress@gmail.com>
2024-08-07 12:24:46 +0100

    next ui: installation

---
+++ src/app/homepage/AddLink.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const AddLink = (props: Props) => {
+  return (
+    <div>This is the link component <p>to add other links slslslslslslslsllslsls slslslslslslslslsls</p>
+    div</div>
+
+  )
+}
+
+export default AddLink
