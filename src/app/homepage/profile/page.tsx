c1fd873b9b418abe06673bf9f692efd820cb30fc
Moluno-xiii <molunoprogress@gmail.com>
2024-08-30 21:15:20 +0100

    feat : implemented logout functionality

--- src/app/homepage/profile/page.tsx
+++ src/app/homepage/profile/page.tsx
@@ -1,17 +1,35 @@
-"use client"
-import React from "react";
-import UploadFile from "./UploadFile";
-import { Button, Input } from "@nextui-org/react";
-import { useSelector } from "react-redux";
-import { RootState } from "@/app/store/store";
+'use client';
+import React from 'react';
+import UploadFile from './UploadFile';
+import { Button, Input } from '@nextui-org/react';
+import { useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/app/store/store';
+import { useDispatch } from 'react-redux';
+import { logout } from '@/app/store/authSlice/authThunks';
+import { useRouter } from 'next/navigation';
 
 type Props = {};
 function Page({}: Props) {
-  const { user, profilePicture } = useSelector(
-    (state: RootState) => state.auth,
-  );
+  const { user } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+
+  const logoutUser = () => {
+    dispatch(logout());
+    router.push('/');
+  };
   return (
     <div className="flex flex-col gap-y-5 p-4">
+      <div className="mx-auto w-[295px] space-y-1 md:w-[640px]">
+        <Button
+          variant="shadow"
+          onClick={logoutUser}
+          className="w-20"
+          color="danger"
+        >
+          Logout
+        </Button>
+      </div>
       <header className="mx-auto w-[295px] space-y-1 md:w-[640px]">
         <p className="text-2xl font-bold text-dark-gray">Profile Details</p>
         <span className="text-base text-grey">
@@ -35,7 +53,7 @@ function Page({}: Props) {
           defaultValue={user.name}
           label="Username"
           placeholder="e.g. Omotola"
-          />
+        />
         <Input
           isRequired
           isDisabled
@@ -46,7 +64,12 @@ function Page({}: Props) {
         />
       </form>
 
-      <Button isDisabled color="primary" variant="shadow" className="mx-auto w-[295px]">
+      <Button
+        isDisabled
+        color="primary"
+        variant="shadow"
+        className="mx-auto w-[295px]"
+      >
         Save
       </Button>
     </div>
